2fb184cb5e12b14c29ef67f906de4862293f2106
Eram2831 <156246166+Eram2831@users.noreply.github.com>
2024-03-06 12:27:38 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,12 +5,48 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission &> cloned-output.txt
 echo 'Finished cloning'
-
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
+cp student-submission/*.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if ! [ -f ListExamples.java ]
+then
+    echo "Missing ListExamples.java in student submission"
+    echo "Score: 0"
+    exit
+fi 
+
+
+javac -cp $CPATH *.java &>compile.txt
+
+if [ $? -ne 0 ]
+then
+    cat compile.txt
+    echo "Compilation Error: see above"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+if grep -q 'E' junit-output.txt
+then
+    lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+    tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+    failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+    successes=$((tests-failures))
+    echo "Number of tests passed: $successes / $tests"
+else 
+    echo All tests passed, Grade is 100
+fi
